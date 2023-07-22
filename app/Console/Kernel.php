9878328d247fe7ed460af5a42b13cff83c100cbb
Anton160 <antoniwahib@gmail.com>
2023-07-22 12:50:45 +0700

    1.0.0

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new AutoAttendance)->dailyAt('09.55');
+        $schedule->job(new AutoAttendance)->dailyAt('10.00');
     }
 
     /**
